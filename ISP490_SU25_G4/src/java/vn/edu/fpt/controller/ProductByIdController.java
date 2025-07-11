a9255213ee7f42fe4842075e9ac7a0ad7ac7409c
Phạm Hải Huy <167501072+huy150303@users.noreply.github.com>
2025-07-11 08:22:34 +0700

    Fix productByIdController

--- ISP490_SU25_G4/src/java/vn/edu/fpt/controller/ProductByIdController.java
+++ ISP490_SU25_G4/src/java/vn/edu/fpt/controller/ProductByIdController.java
@@ -4,14 +4,20 @@ import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import vn.edu.fpt.dao.ProductCategoriesDAO;
 import vn.edu.fpt.dao.ProductDAO;
 import vn.edu.fpt.model.Product;
+import vn.edu.fpt.model.ProductCategory;
 
 @WebServlet(name = "GetProductByIdServlet", urlPatterns = {"/getProductById"})
 public class ProductByIdController extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        ProductCategoriesDAO productCategories = new ProductCategoriesDAO();
         String idStr = request.getParameter("id");
         if (idStr == null || idStr.isEmpty()) {
             response.sendRedirect(request.getContextPath() + "/404.jsp");
@@ -21,8 +27,13 @@ public class ProductByIdController extends HttpServlet {
 
         ProductDAO dao = new ProductDAO();
         Product product = dao.getProductById(id);
-
+        List<ProductCategory> categories = productCategories.getAllCategories();
+        Map<Integer, String> categoryMap = new HashMap<>();
+        for (ProductCategory c : categories) {
+            categoryMap.put(c.getId(), c.getName());
+        }
         if (product != null) {
+            request.setAttribute("categoryMap", categoryMap);
             request.setAttribute("product", product);
             request.getRequestDispatcher("/jsp/technicalSupport/viewProductDetail.jsp").forward(request, response);
         } else {
